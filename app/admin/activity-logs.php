96a46107288052dbe1e45f007c288e47ee01f303
ErickJamesRubin <rubin.erick123@gmail.com>
2026-03-08 11:21:37 +0800

    Changes in activity-logs for admin

--- app/admin/activity-logs.php
+++ app/admin/activity-logs.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../config/function.php';
 require_once __DIR__ . '/../../includes/header.php';
 require_once __DIR__ . '/../../includes/sidebar.php';
+require_once __DIR__ . '/../../includes/footer.php';
 
 requireRole('admin');
 
